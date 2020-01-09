81dfe4548d32d1ad0852512828857558ce0011e9
Armen Boiadzhian <z@fastlive.eu>
2020-01-09 15:42:46 +0100

    Parametrization has been added

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, "#add_to_basket_form button")
+    ITEM_NAME = (By.CSS_SELECTOR, "#content_inner h1")
+    ADDED_ITEM_NAME = (By.CSS_SELECTOR, "#messages strong") #ADDED_ITEM_NAME[0]
+    ITEM_PRICE = (By.CSS_SELECTOR, "#content_inner p")
+    ADDED_ITEM_PRICE = (By.CSS_SELECTOR, "#messages strong") #ADDED_ITEM_PRICE[3]
--- pages/login_page.py
+++ pages/login_page.py
@@ -9,8 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert self.browser.current_url == "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/", \
-            "Wrong URL"
+        assert self.browser.current_url.find("accounts/login/"), "Wrong URL"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
