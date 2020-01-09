06f5efc1fb911078c92efbb4fec22943f73a4e8f
Armen Boiadzhian <z@fastlive.eu>
2020-01-09 10:40:50 +0100

    NoSuchElementException Handling has been added

--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,13 @@ from pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    page = MainPage(browser, link)   # Page Object initialization,
+                                     # pass the driver instance and url address to the constructor
+    page.open()                      # open the page
+    page.go_to_login_page()          # execute method of the page - redirect to the login page
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
