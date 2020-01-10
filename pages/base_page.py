1456c720c83479a4495bc20536bd5a2c552d8a14
Armen Boiadzhian <z@fastlive.eu>
2020-01-10 16:31:55 +0100

    Login test methods has been moved from main_page.py to base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,7 +3,7 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-
+from .locators import BasePageLocators
 import math
 
 
@@ -53,3 +53,13 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def go_to_login_page(self):
+        try:
+            link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+            link.click()
+        except (NoSuchElementException):
+            print("Login link is not presented")
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
--- test_product_page.py
+++ test_product_page.py
@@ -50,3 +50,19 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     add_item.open()
     add_item.add_item_to_bascket()
     add_item.should_not_be_success_message_is_disappeared()
+
+
+link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_link()
